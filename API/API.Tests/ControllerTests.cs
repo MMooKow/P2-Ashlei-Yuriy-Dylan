cec7b7b33d134bf002f6aed106e2a299f4bab1e8
Dylan Restivo <dylan.restivo@yahoo.com>
2021-09-17 14:02:09 -0500

    Tried to set up Moq. Error with Castle.Core

---
+++ API/API.Tests/ControllerTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using API.Controllers;
+
+namespace API.Tests
+{
+    /// <summary>
+    /// Tried installing Moq. Getting error "Failed to add reference. The package 'Castle.Core' tried to add a framework reference to 'System.Configuration' which was not found in the GAC..."
+    /// </summary>
+    public class ControllerTests
+    {
+        //    BuggyController testBuggy = new BuggyController();
+    }
+}
--- Test/API/UnitTest1.cs
+++ Test/API/UnitTest1.cs
@@ -5,10 +5,6 @@ namespace Test.API
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
-        {
 
-        }
     }
 }
