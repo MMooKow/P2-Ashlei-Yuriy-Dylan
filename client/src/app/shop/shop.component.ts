95e4919fc5df862b27e14f8e8cbd1136f2021d89
Yuriy Yarytskyy <yyarytskyy@gmail.com>
2021-09-18 23:39:24 -0400

    Added Search functionality

--- client/src/app/shop/shop.component.ts
+++ client/src/app/shop/shop.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { IBrand } from '../models/brand';
 import { IProduct } from '../models/product';
 import { IType } from '../models/productType';
@@ -11,6 +11,7 @@ import { ShopService } from './shop.service';
   styleUrls: ['./shop.component.css']
 })
 export class ShopComponent implements OnInit {
+  @ViewChild('search') searchTerm: ElementRef;
   products: IProduct[];
   brands: IBrand[];
   types: IType[];
@@ -55,10 +56,12 @@ export class ShopComponent implements OnInit {
   }
   onBrandSelected(brandId: number){
     this.shopParams.brandId = brandId;
+    this.shopParams.pageNumber = 1;
     this.getProducts();
   }
   onTypeSelected(typeId: number){
     this.shopParams.typeId = typeId;
+    this.shopParams.pageNumber = 1;
     this.getProducts();
   }
   onSortSelected(sort: string) {
@@ -66,7 +69,21 @@ export class ShopComponent implements OnInit {
     this.getProducts();
   }
   onPageChanged(event: any){
-    this.shopParams.pageNumber = event;
+    if (this.shopParams.pageNumber !== event){
+      this.shopParams.pageNumber = event;
+      this.getProducts();
+    }
+    
+  }
+
+  onSearch(){
+    this.shopParams.search =this.searchTerm.nativeElement.value;
+    this.shopParams.pageNumber = 1;
+    this.getProducts();
+  }
+  onReset(){
+    this.searchTerm.nativeElement.value = '';
+    this.shopParams = new ShopParams();
     this.getProducts();
   }
 
