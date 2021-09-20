9abb6a466c383bdd9873a0673c02cca7c56cddc4
Yuriy Yarytskyy <yyarytskyy@gmail.com>
2021-09-20 15:56:16 -0400

    initial set up for lazy loading (Inventory/shop)

---
+++ client/src/app/pre-approval/pre-approval.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PreApprovalComponent } from './pre-approval.component';
+
+
+
+@NgModule({
+  declarations: [
+    PreApprovalComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class PreApprovalModule { }
