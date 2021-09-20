1c737276be89b35938171ddb7f4c97f91c7e40b2
Dylan Restivo <dylan.restivo@yahoo.com>
2021-09-20 13:48:35 -0500

    Added Microsoft test sdk package and removed xunit console runner package

---
+++ API.Tests/ControllersTests/BuggyControllerTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using API.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using Xunit;
+
+namespace API.Tests.ControllersTests
+{
+    public class BuggyControllerTests
+    {
+        [Fact]
+        public ActionResult<string> GetSecretText_ValidCall()
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
