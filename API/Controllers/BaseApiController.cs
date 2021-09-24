e6d86c632854315072262eabcbe3d999deb42639
Yuriy Yarytskyy <yyarytskyy@gmail.com>
2021-09-24 01:28:27 -0400

    API: added shopping cart functionality. Using REDIS to store cart. REDIS running on Docker

--- API/Controllers/BaseApiController.cs
+++ API/Controllers/BaseApiController.cs
@@ -2,12 +2,15 @@
 
 namespace API.Controllers
 {
-
+    /// <summary>
+    /// Provides root for endpoints to use with all controllers
+    /// </summary>
     [ApiController]
     [Route("api/[controller]")]
     public class BaseApiController : ControllerBase
     {
 
     }
+
     
 }
