5bcfac91d62a14014f688acc51cc5651fe43b296
Yuriy Yarytskyy <yyarytskyy@gmail.com>
2021-09-25 23:24:00 -0400

    testing if backend visibility fixed

--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -4,7 +4,8 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'https://localhost:44329/api/'
+  // apiUrl: 'https://localhost:44329/api/'
+  apiUrl: 'https://chariotsbackend.azurewebsites.net/'
 };
 
 /*
