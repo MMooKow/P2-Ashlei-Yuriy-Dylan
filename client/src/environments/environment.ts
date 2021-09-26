29c4519a47ca1d79df035886207034a7300ea8a9
ashlie.nguyen <ashlie.nguyen@revature.net>
2021-09-26 00:36:29 -0400

    connect to back end

--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -3,8 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: true,
-  apiUrl: 'https://chariotsbackend.azurewebsites.net/'
+  production: false,
+  apiUrl: 'https://chariotsbackend.azurewebsites.net/api/'
 };
 
 /*
